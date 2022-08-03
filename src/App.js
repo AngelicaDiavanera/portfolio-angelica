59372da117bb4efbdf5f8b0d76d99d4dd06a56e3
AngelicaDiavanera <mdiavanera@gmail.com>
2022-08-02 20:15:11 -0500

    actualizacion de app.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useNavigate } from './utils/useNavigate';
+import { mounted } from './utils/mounted';
+
+export const App = () => {
+  const { navigate } = useNavigate();
+
+  mounted(() => {
+    navigate('/dashboard');
+  });
+
+  return '';
+};
